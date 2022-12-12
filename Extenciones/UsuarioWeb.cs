9768977576273b57adc8ad03a6779897e0862d2d
Luisito Salvarezza <desarrollo1@drrsystemas.com>
2022-12-12 08:48:26 -0300

    Agregar archivos de proyecto.

---
+++ Extenciones/UsuarioWeb.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DRR.Core.DBAlmaNET.Models
+{
+    public partial class UsuarioWeb
+    {
+        [Required(ErrorMessage = "Ingrese el Correo Electrónico."),
+                    EmailAddress(ErrorMessage = "El correo ingresado no es valido.")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Ingrese la clave para el sitio")]
+        [DataType(DataType.Password)]
+        [StringLength(40, MinimumLength = 6, ErrorMessage = "La clave tiene que tener como minimo 6 caracteres")]
+        public string Contrasena { get; set; }
+
+
+        private string _error;
+
+        [NotMapped]
+        public string P_Error
+        {
+            get { return _error; }
+            set { _error = value; }
+        }
+
+    }
+}
